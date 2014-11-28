ed353e8341587321485f8090b3b46b5fb8be2456
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2014-11-28 07:23:57 +0700

    changing debug function

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 var ElasticSearchClient = require('elasticsearchclient');
-var db = new ElasticSearchClient({
+var db                  = new ElasticSearchClient({
     host: 'localhost',
     port: '9200'
 });
 var dateFormats = ['DD+MM+YYYY', 'DD+MMM+YYYY', 'DD MM YYYY', 'DD MMM YYYY'];
-var moment = require('moment');
-var airlines = {"airasia": 1, "citilink": 2, "garuda": 3, "lion": 4, "sriwijaya": 5, "xpress": 6};
-var _debug = 1;
-var debug = function () {
-	if (_debug)
-		console.log.apply(null, arguments)
-}
+var moment      = require('moment');
+var airlines    = {"airasia": 1, "citilink": 2, "garuda": 3, "lion": 4, "sriwijaya": 5, "xpress": 6};
+var debug       = require('debug')('raabbajam:priceInserter:base');
 function priceInserter (airline) {
-	_kode = airlines[airline] || 0;
+	_kode    = airlines[airline] || 0;
 	_airline = airline;
 	return function (dt, price) {
+		if(!!dt.rute && dt.rute.toLowerCase() === 'rt'){
+			console.log('Return route. Not cached!');
+		}
 		insertCache(dt, price);
-		if(!!dt.rute && dt.rute.toLowerCase() === 'ow')
-			insertCalendar(dt, price);
+		insertCalendar(dt, price);
 	}
 }
 function insertCache (dt, price) {
 	var _price = parseInt(price, 10) + _kode;
 	var data = {
-		origin: dt.ori,
+		origin     : dt.ori,
 		destination: dt.dst,
-		airline: _airline,
-		flight: dt.ftid || '',
-		class: dt.class || '',
-		price: _price
+		airline    : _airline,
+		flight     : dt.ftid || '',
+		class      : dt.class || '',
+		price      : _price
 	};
 	data.id = data.origin + data.destination + data.airline + data.flight + data.class;
 	db.index('pluto', 'price', data, function (err, res) {
@@ -37,13 +35,13 @@ function insertCache (dt, price) {
 }
 function insertCalendar (dt, price) {
 	var _price = parseInt(price, 10) + _kode;
-	var _date = moment(dt.dep_date, dateFormats).unix() * 1000;
-	var data = {
-		date: _date,
-		origin: dt.ori,
+	var _date  = moment(dt.dep_date, dateFormats).unix() * 1000;
+	var data   = {
+		date       : _date,
+		origin     : dt.ori,
 		destination: dt.dst,
-		price: _price,
-		airline: _airline,
+		price      : _price,
+		airline    : _airline,
 	};
 	data.id = data.origin + data.destination + Math.round(data.date/1000);
 
